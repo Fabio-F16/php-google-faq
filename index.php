4a68919ced396a48019666fb30e988e89fa28414
Fabio-F16 <fipsfabio95@gmail.com>
2022-05-04 15:15:58 +0200

    Test paragrafi

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ include('data.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Php Google Faq</title>
 </head>
 <body>
@@ -20,8 +21,10 @@ include('data.php');
         <?php
         
        foreach($faq as $item) {
-           echo('<h2>' . $item['question'] . '</h2>');
-           echo('<p>' . $item['answer'] . '</p>');
+           $question = $item['question'];
+           $answer = $item['answer'];
+           echo('<div>' . '<h2>' . $question . '</h2>');
+           echo('<p>' . $answer . '</p>' . '</div>');
 
        }
         ?>
